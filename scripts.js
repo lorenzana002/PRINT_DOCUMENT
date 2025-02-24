9c81f09460a83b14e31b615209e4aa7c7c60ad03
Azael <118006161+lorenzana002@users.noreply.github.com>
2025-02-23 17:15:54 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const documentos = [
         { nombre: 'CV ACTUALIZADO', archivo: 'Documentos/CV_00.pdf' },
-        { nombre: '1', archivo: 'CFE Nov-Enero.pdf' },
+   /*      { nombre: '1', archivo: 'CFE Nov-Enero.pdf' },
         { nombre: '2', archivo: 'IMG_20250223_0002.pdf' },
         { nombre: '3', archivo: 'IMG_20250223_0003.pdf' },
         { nombre: '4', archivo: 'IMG_20250223_0004.pdf' },
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
         { nombre: '9', archivo: 'IMG_20250223_0010.pdf' },
         { nombre: '10', archivo: 'IMG_20250223_0011.pdf' },
         { nombre: '11', archivo: 'IMG_20250223_0012.pdf' },
-        { nombre: '12', archivo: 'IMG_20250223_0013_NEW.pdf' },
-        { nombre: 'EN MANTENIMIENTO 🚧', archivo: 'documentos/documento2.pdf' },
+        { nombre: '12', archivo: 'IMG_20250223_0013_NEW.pdf' }, */
+        { nombre: 'EN MANTENIMIENTO 🚧', archivo: 'referencia_7800820.pdf' },
         { nombre: 'NO DISPONIBLE ☠💀', archivo: 'https://drive.google.com/file/d/1H6AjeSdG7AjsYxjyiSELrPfnJ4BPWhsq/view?usp=sharing' },
     ];
 
